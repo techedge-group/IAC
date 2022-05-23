922b401d60da3d7458d5267d0f488e8371ecf762
hector <hector.arrieta@techedgegroup.com>
2022-05-23 16:35:32 +0200

    media querys

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,22 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    colors: {
-      corp: '#A0CE4E',
-      secondary: '#045B62',
-      test: '#FF0000',
-      darkText: '#1E293B',
-      mainText: '#777777',
-      lightText: '#94A3B8',
+       extend: {
+        colors: {
+          corp: '#A0CE4E',
+          secondary: '#045B62',
+          test: '#FF0000',
+          darkText: '#1E293B',
+          mainText: '#777777',
+          lightText: '#94A3B8',
+        },
+        fontFamily: {
+          'merriweather': ["'Merriweather'", 'serif']
+        },
+        height: {
+          '128': '34rem',
+        }
     },
-    // extend: {},
   },
   plugins: [],
-}
+};
