2dd894b8518ec6f3b319c59e9ac9c4bf2f8e3566
hector <hector.arrieta@techedgegroup.com>
2022-08-01 11:11:46 +0200

    change videos

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -14,6 +14,8 @@ export class LoginComponent implements OnInit {
   userInput:any = [];
   errorEvent:boolean = false;
   error:string = 'Por favor, introduce un usuario y contraseña correcto';
+  inputPwd:any;
+  userPwd:any;
   
   
   constructor(public router: Router) { }
@@ -24,10 +26,13 @@ export class LoginComponent implements OnInit {
 
   login() {
     this.inputValue = document.getElementsByTagName("input")[0].value;
+    this.inputPwd = document.getElementsByTagName("input")[1].value;
     this.userInput = this.inputValue.toString();
-    if(this.userInput.includes('@iac.es') && this.userInput != '') {
+    this.userPwd = this.inputPwd.toString();
+    if(this.userInput.includes('@iac.es') && this.userInput != '' && this.userPwd.includes('inicio01') || this.userInput == 'usuario@fundae.es' && this.userInput != '' && this.userPwd.includes('inicio01'))  {
       this.router.navigateByUrl('/sessions');
     } else {
+      
       this.errorEvent = true; 
     }
   };
